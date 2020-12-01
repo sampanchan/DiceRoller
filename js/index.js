4366636bef38562c33e0ec1e4b134ed143ce6c1f
sampanchan <spankratz@my.creativecircus.edu>
2020-12-01 12:56:40 -0500

    updated

--- js/index.js
+++ js/index.js
@@ -1,57 +1,75 @@
 console.log('hello')
 
 
-//Dice roller (placed at bottom of screen [will have 4 di: 4,6,10, 20])
-// draggable( cursor picks up dice)
-// drops wherever on screen
-//once dropped: twists to random side(face) to show numbers
-
-
-//drag
-
-// let obj = document.querySelector('img')
-// Draggable.create(".place-holder", {type:"x,y", edgeResistance:0.65, bounds:"body", inertia:true});
-// gsap.to(obj, {inertia: {x: 500, y: -300}});
-
+let rollResults = document.querySelector('.roll-results')
 let dice4 = document.querySelector('.d4')
 let dice6 = document.querySelector('.d6')
 let dice8 = document.querySelector('.d8')
 let dice10 = document.querySelector('.d10')
 let dice20 = document.querySelector('.d20')
+let diceImages = document.querySelectorAll('.dice img')
+let yaDumbName = document.querySelector('#name')
+
+
+yaDumbName.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') e.preventDefault()
+})
 
 //submit name inside JS: document write inside .result section
 
 //Dice rolls inside .result section
 // getElementByID
 //"element".innerHtml()
-
+let rollDice = function(sides){
+    return Math.ceil(Math.random()* sides)
+}
 
 dice4.addEventListener('click', function(){
     console.log('dice4')
-    let rollDice4 = function(){
-        let randomOne = 1 + Math.floor(Math.random()* 4)
-        return randomOne
-        
+   
 
-    }
-    console.log(rollDice4(1))
-    let dFour = document.querySelector('.diceResult4')
-    dFour.innerHTML= 'you rolled a ' + rollDice4(1)
+    // rotate image every time  its clicked
+    // diceImages.forEach(function(diceImage) {
+    //    diceImage.classList.add('animation')
+    // })
+
+    this.classList.add('animation')
+    setTimeout(() => {
+        this.classList.remove('animation')
+    }, 1000)
+   
+    var sound = new Howl({
+        src: ['js/roll.wav']
+        });
+        
+        sound.play()
     
+    let newResultLI = document.createElement('li')
+    newResultLI.classList.add('diceResult4')
+    newResultLI.innerHTML= yaDumbName.value + ' rolled a ' + rollDice(4)
+    rollResults.insertBefore(newResultLI, rollResults.firstChild)
+
 })
 
 
 
 dice6.addEventListener('click', function(){
     console.log('dice6')
-    let rollDice6 = function(){
-        let randomTwo = 1 + Math.floor(Math.random()* 6)
-        return randomTwo
+    this.classList.add('animation')
+    setTimeout(() => {
+        this.classList.remove('animation')
+    }, 1000)
 
-    }
-    console.log(rollDice6(1))
-    let dSix = document.querySelector('.diceResult6')
-    dSix.innerHTML= 'you rolled a ' + rollDice6(1)
+    var sound = new Howl({
+        src: ['js/roll.wav']
+        });
+        
+        sound.play()
+
+    let newResultLI = document.createElement('li')
+    newResultLI.classList.add('diceResult6')
+    newResultLI.innerHTML= yaDumbName.value + ' rolled a ' + rollDice(6)
+    rollResults.insertBefore(newResultLI, rollResults.firstChild)
 
 })
 
@@ -59,41 +77,69 @@ dice6.addEventListener('click', function(){
 
 dice8.addEventListener('click', function(){
     console.log('dice8')
-    let rollDice8 = function(){
-        let randomThree = 1 + Math.floor(Math.random()* 8)
-        return randomThree
+    this.classList.add('animation')
+    setTimeout(() => {
+        this.classList.remove('animation')
 
-    }
-    console.log(rollDice8(1))
-    let dEight = document.querySelector('.diceResult8')
-    dEight.innerHTML= 'you rolled a ' + rollDice8(1)
+    }, 1000)
+
+
+    //dice roll sound
+    var sound = new Howl({
+        src: ['js/roll.wav']
+        });
+        
+        sound.play()
+
+    let newResultLI = document.createElement('li')
+    newResultLI.classList.add('diceResult8')
+    newResultLI.innerHTML= yaDumbName.value + ' rolled a ' + rollDice(8)
+    rollResults.insertBefore(newResultLI, rollResults.firstChild)
 })
 
 
 
 dice10.addEventListener('click', function(){
     console.log('dice10')
-    let rollDice10 = function(){
-        let randomFour = 1 + Math.floor(Math.random()* 10)
-        return randomFour
+    this.classList.add('animation')
+    setTimeout(() => {
+        this.classList.remove('animation')
+    }, 1000)
+
+    var sound = new Howl({
+        src: ['js/roll.wav']
+        });
         
-    }
-    console.log(rollDice10(1))
-    let dTen = document.querySelector('.diceResult10')
-    dTen.innerHTML= 'you rolled a ' + rollDice10(1)
+        sound.play()
+
+    let newResultLI = document.createElement('li')
+    newResultLI.classList.add('diceResult10')
+    newResultLI.innerHTML= yaDumbName.value + ' rolled a ' + rollDice(10)
+    rollResults.insertBefore(newResultLI, rollResults.firstChild)
 })
 
 
 
 dice20.addEventListener('click', function(){
     console.log('dice20')
-    let rollDice20 = function(){
-        let randomFive = 1 + Math.floor(Math.random()* 20)
-        return randomFive
+    this.classList.add('animation')
+    setTimeout(() => {
+        this.classList.remove('animation')
+    }, 1000)
 
-    }
-    console.log(rollDice20(1))
-    let dTwenty = document.querySelector('.diceResult20')
-    dTwenty.innerHTML= 'you rolled a ' + rollDice20(1)
+    var sound = new Howl({
+        src: ['js/roll.wav']
+        });
+        
+        sound.play()
+    
+
+    let newResultLI = document.createElement('li')
+    newResultLI.classList.add('diceResult20')
+    newResultLI.innerHTML= yaDumbName.value + ' rolled a ' + rollDice(20)
+    rollResults.insertBefore(newResultLI, rollResults.firstChild)
 
 })
+
+
+
